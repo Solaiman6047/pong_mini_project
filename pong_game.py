e4e46fffe9c45445310b713d2d7a1adff27b2b50
Hana Mohamed Solaiman <149798813+Solaiman6047@users.noreply.github.com>
2024-07-30 22:17:17 +0300

    Update pong_game.py

--- pong_game.py
+++ pong_game.py
@@ -1,3 +1,34 @@
+#function to move the ball
+def ball_movement():
+    y=ball.ycor()
+    x=ball.xcor()
+    ball.setx(x+ball.dx)
+    ball.sety(y+ball.dy)
+    
+# bouncing the ball against the wall  
+def ball_bouncing():
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    if ball.xcor() > 390:  
+        ball.goto(0,0)
+        ball.dx *= -1
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        
+#function to bounce the ball against the paddle:
+def ball_and_paddle():
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
+    
 #function to move padel_a up
 def paddle_a_up():
     y=paddle_a.ycor()
@@ -38,6 +69,16 @@ paddle_b.shapesize(5,1)
 paddle_b.color("green")
 paddle_b.penup()
 paddle_b.goto(350, 0)
+
+# ball
+ball= turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("red")
+ball.penup()
+ball.goto(0, 0)
+ball.dx=2
+ball.dy=2
     
 #press Up to move padel_a up
 wn.listen()
@@ -49,5 +90,9 @@ wn.onkeypress(paddle_a_down,"Down")
 
 # Main game loop
 while True:
+    ball_movement()
+    ball_bouncing()
+    ball_and_paddle()
     wn.update()  
     time.sleep(0.01)  
+    
