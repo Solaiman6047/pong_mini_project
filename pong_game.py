41705980a398b0f7cefab316f94ad23e9690ba5b
Malak221 <luka.forsure@gmail.com>
2024-08-07 14:25:47 +0300

    5last elcode kda tmam

--- pong_game.py
+++ pong_game.py
@@ -1,9 +1,17 @@
+#made by:[hana mohamed solaiman,malak alaa hassan,jannat abd ellatif]
 import turtle
 import time
+import random
+# Background screen set up
+wn = turtle.Screen()
+wn.bgcolor("light blue")
+wn.title("PONG GAME")
+wn.setup(width=800, height=600)
+wn.tracer(0)
 # Score variables
 score_1 = 0
 score_2 = 0
-winning_score=5
+winning_score=1
 
 # Scoring system
 pen = turtle.Turtle()
@@ -22,6 +30,7 @@ def ball_movement():
 
  #bouncing the ball against the wall  
 def ball_bouncing():
+    global score_1,score_2
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
@@ -31,9 +40,46 @@ def ball_bouncing():
     if ball.xcor() > 390:  
         ball.goto(0,0)
         ball.dx *= -1
+        score_1+=1
+        pen.clear()
+        pen.write(f"You: {score_1}  Computer: {score_2}".format(score_1,score_2),align='center',font=('courier',23,'normal'))
+        winning()
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_2+=1
+        pen.clear()
+        pen.write(f"You: {score_1}  Computer: {score_2}".format(score_1,score_2),align='center',font=('courier',23,'normal'))
+        winning()
+
+def winning():
+    if score_1>=winning_score:
+        pen.clear()
+        pen.write('YOU WIN :)', align='center', font=('courier', 30, 'normal'))
+        wn.update()  
+        time.sleep(3)
+        pen.clear()
+        restart()
+        
+        
+    elif score_2>=winning_score: 
+        pen.clear()
+        pen.write('GAME OVER :(', align='center', font=('courier', 30, 'normal'))
+        wn.update()
+        time.sleep(3)
+        pen.clear()
+        restart()
+        
+        
+def restart():
+    global score_1, score_2
+    score_1 = 0
+    score_2 = 0
+    ball.goto(0, 0)
+    ball.dx = 2.5
+    ball.dy = 2.5
+    pen.clear()
+    pen.write(f"You: {score_1}  Computer: {score_2}".format(score_1,score_2),align='center',font=('courier',23,'normal'))
         
 #function to bounce the ball against the paddle:
 def ball_and_paddle():
@@ -50,24 +96,32 @@ def move_ball():
 def paddle_a_up():
     y=paddle_a.ycor()
     y+=20
+    if y>250:
+        y=250
     paddle_a.sety(y)
     
 #function to move padel_a down
 def paddle_a_down():
     y=paddle_a.ycor()
     y-=20
+    if y<-250:
+        y=-250
     paddle_a.sety(y)
 
 #function to move padel_b up
 def paddle_b_up():
     y=paddle_b.ycor()
     y+=8
+    if y>250:
+        y=250
     paddle_b.sety(y)
 
 #function to move padel_b down
 def paddle_b_down():
     y=paddle_b.ycor()
     y-=8
+    if y<-250:
+        y=-250
     paddle_b.sety(y)
 
 #ai implementation
@@ -89,12 +143,7 @@ def calculate_ball_trajectory():
 import turtle
 import time
 
-# Background screen set up
-wn = turtle.Screen()
-wn.bgcolor("blue")
-wn.title("PONG GAME")
-wn.setup(width=800, height=600)
-wn.tracer(0)
+
 
 
 # paddle_a
@@ -102,7 +151,7 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.shapesize(5,1)
-paddle_a.color("green")
+paddle_a.color("blue")
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
@@ -122,8 +171,8 @@ ball.shape("circle")
 ball.color("red")
 ball.penup()
 ball.goto(0, 0)
-ball.dx=2
-ball.dy=2
+ball.dx=2.5
+ball.dy=2.5
     
 #press Up to move padel_a up
 wn.listen()
@@ -135,13 +184,18 @@ wn.onkeypress(paddle_a_down,"Down")
 
 #ai movement
 def ai_movement():
-    offset=10
+    offset=35
     if ball.heading()<90 or ball.heading()>270:
         target_y=calculate_ball_trajectory()[-1]
-        if target_y>paddle_b.ycor()+offset:
+        if random.random()<0.4:
+         if target_y>paddle_b.ycor()+offset:
             paddle_b.sety(paddle_b.ycor()+5)
-        elif target_y<paddle_b.ycor()+offset:
+         elif target_y<paddle_b.ycor()+offset:
             paddle_b.sety(paddle_b.ycor()-5)
+    if paddle_b.ycor() > 250:
+            paddle_b.sety(250)
+    elif paddle_b.ycor() < -250:
+            paddle_b.sety(-250)
 
 # Main game loop
 while True:
